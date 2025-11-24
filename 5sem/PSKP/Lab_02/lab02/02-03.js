eaa5af1d6673a387735899a0cd00f3e9ec97aff1
RitaRublevskaya06 <144614540+RitaRublevskaya06@users.noreply.github.com>
2025-11-24 13:58:51 +0300

    Add files via upload

---
+++ 5sem/PSKP/Lab_02/lab02/02-03.js
@@ -0,0 +1,13 @@
+let http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === "/api/name" && req.method === 'GET') {
+        res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8;"});
+        res.end("Рублевская Маргарита Владимировна");
+
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html; charset=utf-8;"});
+        res.end("<h2>Not found</h2>");
+    }
+}).listen(5000, () => console.log("Server is running at http://localhost:5000"));
+
