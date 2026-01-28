e4905638e0f83e493daaae1d77ee09dbafa486da
RitaRublevskaya06 <144614540+RitaRublevskaya06@users.noreply.github.com>
2026-01-28 23:35:03 +0300

    Add files via upload

---
+++ 5sem/SP/Lab_02/SP_Lab11/OS11_CREATE/OS11_CREATE.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <wchar.h>
+#include <string>
+#pragma comment(lib, "../x64/Debug/OS11_HTAPI.lib")
+
+#include "../OS11_HTAPI/HT.h"
+
+
+int main(int argc, char* argv[])
+{
+	int capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength;
+	wchar_t* fileName;
+
+	try
+	{
+		if (argv[1] && argv[2] && argv[3] && argv[4] && argv[5])
+		{
+			capacity = std::stoi(argv[1]);
+			snapshotIntervalSec = std::stoi(argv[2]);
+			maxKeyLength = std::stoi(argv[3]);
+			maxPayloadLength = std::stoi(argv[4]);
+
+			const size_t cSize = strlen(argv[5]) + 1;
+			wchar_t* wc = new wchar_t[cSize];
+			mbstowcs(wc, argv[5], cSize);
+
+			fileName = wc;
+		}
+		else
+		{
+			throw "Invalid command line arguments";
+		}
+		HT::HTHANDLE* HT;
+		HT = HT::Create(capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength, fileName);
+		if (HT == NULL)
+			throw "Invalid handle";
+		HT::Close(HT);
+		std::cout << "HT-Storage Created " << std::endl;
+		std::wcout << "Filepath: " << fileName << std::endl;
+		std::cout << "SnapshotIntervalSec: " << snapshotIntervalSec << std::endl;
+		std::cout << "Capacity: " << capacity << std::endl;
+		std::cout << "MaxKeyLength: " << maxKeyLength << std::endl;
+		std::cout << "MaxPayloadLength: " << maxPayloadLength << std::endl;
+	}
+	catch (const char* err)
+	{
+		std::cout << err << std::endl;
+		return -1;
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Произошла ошибка. Проверьте параметры и повторите попытку" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
---
+++ 5sem/SP/Lab_02/SP_Lab11/OS11_START/OS11_START.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <wchar.h>
+#include <string>
+#include <conio.h>
+
+#pragma comment(lib, "../x64/Debug/OS11_HTAPI.lib")
+#include "../OS11_HTAPI/HT.h"
+
+int main(int argc, char* argv[])
+{
+	wchar_t* fileName;
+	try
+	{
+		if (argv[1])
+		{
+			const size_t cSize = strlen(argv[1]) + 1;
+			wchar_t* wc = new wchar_t[cSize];
+			mbstowcs(wc, argv[1], cSize);
+			fileName = wc;
+		}
+		else
+		{
+			throw "Invalid file name";
+		}
+
+		HT::HTHANDLE* ht;
+		ht = HT::Open(fileName);
+
+		if (ht)
+		{
+			std::cout << "HT-Storage Created " << std::endl;
+			std::wcout << "Filename: " << fileName << std::endl;
+			std::cout << "SnapshotIntervalSec: " << ht->SecSnapshotInterval << std::endl;
+			std::cout << "Capacity: " << ht->Capacity << std::endl;
+			std::cout << "MaxKeyLength: " << ht->MaxKeyLength << std::endl;
+			std::cout << "MaxPayloadLength: " << ht->MaxPayloadLength << std::endl;
+
+			while (!_kbhit())
+				SleepEx(0, TRUE);
+
+			HT::Close(ht);
+		}
+		else
+			throw "Error while opening a storage";
+	}
+	catch (const char* err)
+	{
+		std::cout << err << std::endl;
+		return -1;
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Произошла ошибка. Проверьте параметры и повторите попытку" << std::endl;
+		return -1;
+	}
+	return 0;
+}
